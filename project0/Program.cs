c99fb1295049ccc351a4c910d765802a72d4ebe7
Yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2023-09-12 12:48:56 +0300

    Add project files.

---
+++ project0/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random r = new();
+        float number1 = r.NextSingle() * 100 - 50, number2 = r.NextSingle() * 100 - 50;
+
+        Console.WriteLine("Calculate: {0} * {1} = {2}", number1, number2, ProductNumb(number1, number2));
+
+    }
+
+    /// <summary>
+    /// Функция вычисления произведения двух вещественных чисел
+    /// </summary>
+    /// <param name="number1">Первый множитель</param>
+    /// <param name="number2">Второй множитель</param>
+    /// <returns>Произведение</returns>
+    public static float ProductNumb(float number1, float number2)
+    {
+        return number1 * number2;
+    }
+}
