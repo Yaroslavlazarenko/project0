10bd9af82396fc9be8c62358d55ab4e4e15b0fe1
Yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2023-09-12 13:27:29 +0300

    edin name2

--- project0/Program.cs
+++ project0/Program.cs
@@ -5,7 +5,7 @@
         Random r = new();
         float number1 = r.NextSingle() * 100 - 50, number2 = r.NextSingle() * 100 - 50;
 
-        Console.WriteLine("Calculate: {0} number2<0?-:+ {1} = {2}", number1, number2, MultiplicationNumb(number1, number2));
+        Console.WriteLine("Calculate: {0} number2<0?-:+ {1} = {2}", number1, number2, MultiplicationNumbers(number1, number2));
 
     }
 
@@ -15,7 +15,7 @@
     /// <param name="number1">Первый множитель</param>
     /// <param name="number2">Второй множитель</param>
     /// <returns>Произведение</returns>
-    public static float MultiplicationNumb(float number1, float number2)
+    public static float MultiplicationNumbers(float number1, float number2)
     {
         return number1 * number2;
     }
